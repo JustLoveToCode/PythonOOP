9af4ba5760fe96b16f585265847c12a5031bd8d7
JustLoveToCode <135109684+JustLoveToCode@users.noreply.github.com>
2024-01-02 14:04:30 +0800

    Add files via upload
    This is the first commit

---
+++ Python OOP-Coffee Machine/main.py
@@ -0,0 +1,30 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+# Print the report
+coffee_maker.report()
+money_machine.report()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f'What would you like?\n{options},\nTurn off the machine by inputting:"off",\n'
+                   f'See your report by inputting: "report": ')
+    # Turn off the machine:
+    if choice == 'off':
+        is_on = False
+    # See the report for the profit and amount of ingredients:
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
